8f1aa0968b8c6e2526da18c2c4b94799f91ff0b2
programmer-cc <2140130341@qq.com>
2026-06-20 18:20:57 +0800

    chore 暂时删除 ImmersingLinker.UI（真的是暂时吗）

--- ImmersingLinker.UI/ViewModels/StudentViewerWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace ImmersingLinker.UI.ViewModels;
-
-public class StudentViewerWindowViewModel
-{
-    
-}
--- ImmersingLinker.UI/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace ImmersingLinker.UI.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
